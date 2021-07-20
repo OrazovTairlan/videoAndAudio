5df0c3682f12584e7723748684fb6bb8794a0916
Qjawko <Qjawko@gmail.com>
2021-07-20 15:46:11 +0600

    fixed width flex elems

--- src/components/layouts/Devices.js
+++ src/components/layouts/Devices.js
@@ -9,14 +9,59 @@ class Devices extends Component {
                 </div>
                 <div class="devices-row">
                     <div class="devices-item">
-                        <img src = "../../images/device-success.svg"/>
-                        <p class="devices-item__text">
+                        <img src = "../../images/device-success.svg" className="devices-item__image"/>
+                        <p class="devices-item__title">
                             Подключение
                             веб-камеры
                         </p>
                         <p class="devices-item__status">
                             Успешно
                         </p>
+                        <button class="devices-repeat">
+                            <img src="../../images/button-again.svg" alt="" class="devices-repeat-img"/> <span class="devices-repeat-text">Начать заново</span>
+                        </button>
+                    </div>
+                    <div className="devices-item">
+                        <img src="../../images/device-success.svg" className="devices-item__image"/>
+                        <p className="devices-item__title">
+                            Подключение
+                            веб-камеры
+                        </p>
+                        <p className="devices-item__status">
+                            Успешно
+                        </p>
+                        <button className="devices-repeat">
+                            <img src="../../images/button-again.svg" alt="" className="devices-repeat-img"/> <span
+                            className="devices-repeat-text">Начать заново</span>
+                        </button>
+                    </div>
+                    <div className="devices-item">
+                        <img src="../../images/device-success.svg" className="devices-item__image"/>
+                        <p className="devices-item__title">
+                            Подключение
+                            веб-камеры
+                        </p>
+                        <p className="devices-item__status">
+                            Успешно
+                        </p>
+                        <button className="devices-repeat">
+                            <img src="../../images/button-again.svg" alt="" className="devices-repeat-img"/> <span
+                            className="devices-repeat-text">Начать заново</span>
+                        </button>
+                    </div>
+                    <div className="devices-item">
+                        <img src="../../images/device-success.svg" className="devices-item__image"/>
+                        <p className="devices-item__title">
+                            Подключение
+                            веб-камеры
+                        </p>
+                        <p className="devices-item__status">
+                            Успешно
+                        </p>
+                        <button className="devices-repeat">
+                            <img src="../../images/button-again.svg" alt="" className="devices-repeat-img"/> <span
+                            className="devices-repeat-text">Начать заново</span>
+                        </button>
                     </div>
                 </div>
             </div>

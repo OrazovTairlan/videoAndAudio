d63b27b93d575b95d182b09af418b9f960202a06
Qjawko <Qjawko@gmail.com>
2021-07-20 12:57:11 +0600

    inital

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import Introduction from "./Introduction";
+import Devices from "./Devices";
 
 class Main extends Component {
     render() {
+        const {children} = this.props;
         return (
             <div className="main">
-
+                <div className="container">
+                    {children}
+                    <Devices/>
+                </div>
             </div>
         );
     }
